eb4abd5f98bca564825ca49f8efd0e11241ad9c7
yusun <yousun2286@naver.com>
2022-06-06 23:48:03 +0900

    백준 최소 힙 1927

---
+++ 32주차/백준_32주차_최소힙/소스.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int n;
+
+	priority_queue<int, vector<int>, greater<int>> pq;
+
+	scanf("%d", &n);
+
+	int x;
+	while (n != 0) {
+		scanf("%d", &x);
+
+		if (x == 0) {
+			if (pq.empty()) {
+				printf("0\n");
+				n--;
+				continue;
+			}
+			else {
+				printf("%d\n", pq.top());
+				pq.pop();
+			}
+		}
+
+		else {
+			pq.push(x);
+		}
+
+		n--;
+	}
+
+	return 0;
+}
